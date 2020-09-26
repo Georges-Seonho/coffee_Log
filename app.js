61d27b83cd4052e98f3286ec97cc5a7be31b7fd7
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-26 17:35:09 +0200

    basic setup done

--- app.js
+++ app.js
@@ -1,41 +1,83 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+require("./config/dbConnection");
+require("./helpers/hbs-helpers");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("connect-flash");
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(
+  session({
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {},
+  })
+); // Creates a session object, gives a cookie to client that the client sends back on every request
+app.use(flash());
+
+app.use(function (req, res, next) {
+  res.locals.error_message = req.flash("error");
+  res.locals.success_message = req.flash("success");
+  next();
+});
+
+// app.use(function (req, res, next) {
+//   if (req.session.currentUser) {
+//     res.locals.isLoggedIn = true;
+//     res.locals.isAdmin = req.session.currentUser.role === "admin";
+//     res.locals.username = req.session.currentUser.username;
+//   } else {
+//     res.locals.isLoggedIn = false;
+//     res.locals.username = null;
+//     res.locals.isAdmin = false;
+//   }
+//   next();
+// });
+
+app.use("/", require("./routes/index"));
+app.use("/user", require("./routes/users"));
+app.use("/collection", require("./routes/collection"));
+app.use("/coffee", require("./routes/coffees"));
+app.use("/logs", require("./routes/logs"));
+app.use("/dashboard", require("./routes/dashboard"));
+app.use("/auth", require("./routes/auth"));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/collection.js
+++ routes/collection.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home');
+router.get('/:cat', function(req, res, next) {
+  res.render('collection');
 });
 
 module.exports = router;
