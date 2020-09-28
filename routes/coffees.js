dbb40aa9d05e5669025287eb2b453f7f256ac1f5
Francois Vieux <francois.g.vieux@gmail.com>
2020-09-28 11:43:42 +0200

    coffee creation available

--- routes/coffees.js
+++ routes/coffees.js
@@ -1,10 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Coffee = require('../models/Coffee')
 
 router.get('/create', (req, res, next) => res.render('./coffees/new_coffee'));
 
 router.post('/create', (req, res, next) => {
-
-})
+    try {
+        Coffee.create(req.body);
+        res.redirect('/');
+    }
+    catch(err) { next(err);
+    }
+});
 
 module.exports = router;
