8c30625864059a05d865d6b5ae34242bed83ae4d
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-28 09:39:58 +0200

    change views naming

--- routes/logs.js
+++ routes/logs.js
@@ -1,4 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+
+router.get("/create", (req, res, next) => {
+  res.render("logs/new_log");
+});
 
 module.exports = router;
