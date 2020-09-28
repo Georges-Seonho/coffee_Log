e90d179d863f86b39b1114564b90a2822a0e534a
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-28 18:43:26 +0200

    signup and logout

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,44 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+
+router.get("/signup", (req, res, next) => {
+  try {
+    res.render("auth/signup");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/signup", async (req, res, next) => {
+  const { firstname, lastname, email, password } = req.body;
+  try {
+    const foundUser = await User.findOne({ email: email });
+    if (foundUser) {
+      return res.render("auth/signup", { error: "This email already exists!" });
+    }
+
+    const salt = 10;
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    await User.create({
+      firstname,
+      lastname,
+      email,
+      password: hashedPassword,
+    });
+    res.redirect("/");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
