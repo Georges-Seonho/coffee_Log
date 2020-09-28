4f7864f0cd0337d627e3c2fb75e67ee04d087e2a
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-28 12:58:20 +0200

    WIP: create a new coffee inside new log form

--- public/js/new-log.js
+++ public/js/new-log.js
@@ -2,6 +2,16 @@ const newLogNextBtn = document.getElementById("new-log-next-btn");
 const newLogBackBtn = document.getElementById("new-log-back-btn");
 const formLogTechPage = document.getElementById("form-log-tech-page");
 const formLogFeedbackPage = document.getElementById("form-log-feedback-page");
+const addNewCoffeeBtn = document.getElementById("add-new-coffee-btn");
+const addNewCoffeePage = document.getElementById("add-new-coffee");
+
+const inputName = document.getElementById("coffee-name");
+const inputCountry = document.getElementById("coffee-country");
+const inputRegion = document.getElementById("coffee-region");
+const inputAltitude = document.getElementById("coffee-altitude")
+
+const saveCoffeeNewLogBtn = document.getElementById("save-coffee-new-log");
+const saveCoffeeSubmitBtn = document.getElementById("save-coffee-btn");
 
 function changePage() {
   formLogFeedbackPage.classList.toggle("hidden");
@@ -9,5 +19,38 @@ function changePage() {
   newLogBackBtn.classList.toggle("hidden");
 }
 
+function displayNewCoffeePage() {
+  formLogTechPage.classList.toggle("hidden");
+  addNewCoffeePage.classList.toggle("hidden");
+  saveCoffeeNewLogBtn.classList.toggle("hidden");
+  saveCoffeeSubmitBtn.classList.toggle("hidden");
+}
+
+function createCoffee() {
+  // go back to new log page
+  addNewCoffeePage.classList.toggle("hidden");
+  formLogTechPage.classList.toggle("hidden");
+
+  //loop through drying method, flavor and roast => if isChecked, save value into a const (ex: selectedRoast) => use it for the post request
+
+  axios
+    .post("/coffee/create", {
+      name: inputName.value,
+      country: inputCountry.value,
+      region: inputRegion.value,
+      altitude: inputAltitude.value
+      // do this for all create coffee input...
+    })
+    .then((apiResponse) => {
+      console.log(apiResponse);
+      // add the new coffee to the list...
+    })
+    .catch((apiError) => {
+      console.log(apiError);
+    });
+}
+
 newLogNextBtn.addEventListener("click", changePage);
 newLogBackBtn.addEventListener("click", changePage);
+addNewCoffeeBtn.addEventListener("click", displayNewCoffeePage);
+saveCoffeeNewLogBtn.addEventListener("click", createCoffee);
