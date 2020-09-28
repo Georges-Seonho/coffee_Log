317f042959b263f3c34f9a88496bff87ed5aacc6
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-28 16:33:08 +0200

    create coffee inside new log form done

--- routes/coffees.js
+++ routes/coffees.js
@@ -1,16 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Coffee = require('../models/Coffee')
+const Coffee = require("../models/Coffee");
 
-router.get('/create', (req, res, next) => res.render('./coffees/new_coffee'));
+router.get("/create", (req, res, next) => res.render("./coffees/new_coffee"));
 
-router.post('/create', (req, res, next) => {
-    try {
-        Coffee.create(req.body);
-        res.redirect('/');
-    }
-    catch(err) { next(err);
-    }
+router.post("/create", (req, res, next) => {
+  try {
+    const newCoffee = req.body;
+    console.log(newCoffee);
+    Coffee.create(newCoffee);
+    res.redirect("/");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/api/create", (req, res, next) => {
+  try {
+    const newCoffee = req.body;
+    console.log(newCoffee);
+    Coffee.create(newCoffee);
+    res.json(newCoffee);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
