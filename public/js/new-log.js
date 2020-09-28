7203eb26e35d473c18d6df9a695e7cb146b15fb5
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-28 11:57:55 +0200

    add a back btn on new log form page 2

--- public/js/new-log.js
+++ public/js/new-log.js
@@ -1,10 +1,13 @@
 const newLogNextBtn = document.getElementById("new-log-next-btn");
+const newLogBackBtn = document.getElementById("new-log-back-btn");
 const formLogTechPage = document.getElementById("form-log-tech-page");
 const formLogFeedbackPage = document.getElementById("form-log-feedback-page");
 
-function goToNextPage() {
+function changePage() {
   formLogFeedbackPage.classList.toggle("hidden");
   formLogTechPage.classList.toggle("hidden");
+  newLogBackBtn.classList.toggle("hidden");
 }
 
-newLogNextBtn.addEventListener("click", goToNextPage);
+newLogNextBtn.addEventListener("click", changePage);
+newLogBackBtn.addEventListener("click", changePage);
