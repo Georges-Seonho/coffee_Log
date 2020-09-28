0ce3f012d589d04387818e8e28bd223018defdd9
Francois Vieux <francois.g.vieux@gmail.com>
2020-09-28 16:29:54 +0200

    done with collections

--- routes/coffees.js
+++ routes/coffees.js
@@ -2,15 +2,33 @@ const express = require('express');
 const router = express.Router();
 const Coffee = require('../models/Coffee')
 
+//C
 router.get('/create', (req, res, next) => res.render('./coffees/new_coffee'));
 
 router.post('/create', (req, res, next) => {
     try {
         Coffee.create(req.body);
-        res.redirect('/');
+        res.redirect('/collection');
     }
     catch(err) { next(err);
     }
 });
+
+//R
+
+//U 
+router.get('/create', (req, res, next) => res.render('./coffees/edit_coffee'));
+
+router.post('/create', (req, res, next) => {
+    try {
+        Coffee.create(req.body);
+        res.redirect('/collection');
+    }
+    catch(err) { next(err);
+    }
+});
+
+//D
+
 
 module.exports = router;
