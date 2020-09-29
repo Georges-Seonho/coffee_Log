ebdd56c765b1b4dcf6067f793c6ee0b79a2f97d8
Francois Vieux <francois.g.vieux@gmail.com>
2020-09-29 10:08:24 +0200

    done delete user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,16 @@
 var express = require('express');
+const User = require('../models/User');
 var router = express.Router();
+
+// D 
+router.get('/:id/delete', async (req, res, next) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.redirect("/collection/users");
+  } catch (err) {
+    next(err);
+  }
+});
 
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
