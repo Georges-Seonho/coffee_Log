c1e612771e67c2b783b42462153a9d8c4dc9b8c2
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-29 10:05:13 +0200

    WIP: user profile page

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const User = require("../models/User");
 
 /* GET users listing. */
-router.get('/:id', function(req, res, next) {
-  res.render('user-profile');
+router.get("/:id", async (req, res, next) => {
+  try {
+    const currentUser = await User.findById(req.params.id);
+    res.render("user_profile", { currentUser });
+  } catch (err) {
+    next(err);
+  }
 });
-
 
 module.exports = router;
