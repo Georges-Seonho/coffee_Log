fa85c1618a50f9ccd73f2bd99d4288d573fb8eb9
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-30 22:41:46 +0200

    big graph on dashboard + cards css

--- public/js/chartsJs.js
+++ public/js/chartsJs.js
@@ -7,26 +7,34 @@ const getRatioData = () =>
     return result.data;
   });
 
-chartIt();
+  chartRatioSatisfaction();
 
-async function chartIt() {
+async function chartRatioSatisfaction() {
   let result = await getRatioData();
-  console.log(result);
   let date = result.datesData;
   let ratio = result.ratioData;
+  let rates = result.ratesData;
   console.log(ratio, date);
   const ctx = document.getElementById("myChart").getContext("2d");
-  const myChart = new Chart(ctx, {
+  const ratioAndSatisfactionChart = new Chart(ctx, {
     type: "line",
     data: {
       labels: date,
       datasets: [
         {
-          label: "ratio coffee/water in % for each of your logs",
+          label: "coffee/water ratio in %",
+          yAxisID: "A",
           data: ratio,
+          backgroundColor: ["rgba(241,197,63, 0.2)"],
+          borderColor: ["rgba(241,197,63, 1)"],
+          borderWidth: 1,
+        },
+        {
+          label: "satisfaction on a 5points scale",
+          data: rates,
+          yAxisID: "B",
           fill: false,
-          backgroundColor: ["rgba(255, 99, 132, 0.2)"],
-          borderColor: ["rgba(255, 99, 132, 1)"],
+          borderColor: ["rgba(255,62,47, 1)"],
           borderWidth: 1,
         },
       ],
@@ -34,7 +42,41 @@ async function chartIt() {
     options: {
       title: {
         display: true,
-        text: "Ratio coffee/water in % for each of your logs",
+        text: "Coffee/water ratio and satisfaction for each of your logs",
+      },
+      legend: {
+        display: true,
+        position: "bottom",
+      },
+      scales: {
+        yAxes: [
+          {
+            gridLines: {
+              display: false,
+            },
+            id: "A",
+            type: "linear",
+            position: "left",
+            ticks: {
+              max: 40,
+              min: 0,
+              stepSize: 10,
+            },
+          },
+          {
+            gridLines: {
+              display: false,
+            },
+            id: "B",
+            type: "linear",
+            position: "right",
+            ticks: {
+              max: 5,
+              min: 0,
+              stepSize: 1,
+            },
+          },
+        ],
       },
     },
   });
