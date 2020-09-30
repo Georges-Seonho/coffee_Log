3d8566b0bb96fdcccaf3974f09b607a8fe891efc
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-30 19:19:47 +0200

    WIP dashboard

--- public/js/chartsJs.js
+++ public/js/chartsJs.js
@@ -1,28 +1,29 @@
-const getWaterData = () =>
-  axios.get("/dashboard/api/waterData").then((result) => {
-    return result.data;
-  });
+let ratio;
+let dates;
 
-const getCoffeeQtyData = () =>
-  axios.get("/dashboard/api/coffeeQtyData").then((result) => {
+const getRatioData = () =>
+  axios.get("/dashboard/api/ratioData").then((result) => {
+    console.log(result.data.datesData);
     return result.data;
   });
 
 chartIt();
 
 async function chartIt() {
-  let waterData = await getWaterData();
-  let coffeeQtyData = await getCoffeeQtyData();
-  console.log(waterData);
+  let result = await getRatioData();
+  console.log(result)
+  let date = result.datesData;
+  let ratio = result.ratioData;
+  console.log(ratio, date);
   const ctx = document.getElementById("myChart").getContext("2d");
   const myChart = new Chart(ctx, {
     type: "line",
     data: {
-      labels: waterData,
+      labels: date,
       datasets: [
         {
           label: "# of Votes",
-          data: coffeeQtyData,
+          data: ratio,
           backgroundColor: ["rgba(255, 99, 132, 0.2)"],
           borderColor: ["rgba(255, 99, 132, 1)"],
           borderWidth: 1,
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,33 +1,39 @@
 const express = require("express");
 const router = express.Router();
-const Log = require('../models/Log');
-const requireAuth = require('../middlewares/requireAuth');
+const Log = require("../models/Log");
+const requireAuth = require("../middlewares/requireAuth");
 
-router.get("/",requireAuth, function (req, res, next) {
-  res.render("dashboard", {js: ["chartsJs"]});
+router.get("/", requireAuth, function (req, res, next) {
+  res.render("dashboard", { js: ["chartsJs"] });
 });
 
-router.get('/api/waterData', async (req, res, next)=> {
+router.get("/api/ratioData", async (req, res, next) => {
   try {
-    const currentUserId = req.session.currentUser._id; 
-    const userLogs = await Log.find({ user : currentUserId });
-    const waterDatas = [];
-    userLogs.forEach(elm => waterDatas.push(elm.waterQty));
-    res.json(waterDatas);
-  } catch(err) {
-    next(err)
+    const currentUserId = req.session.currentUser._id;
+    const userLogs = await Log.find({ user: currentUserId });
+    let ratioData = [];
+    let datesData = [];
+    userLogs.forEach((elm) =>
+      ratioData.push((elm.coffeeQty / elm.waterQty) * 100)
+    );
+    userLogs.forEach((elm) => {
+      datesData.push(elm.date);
+    });
+    res.json({ ratioData, datesData });
+  } catch (err) {
+    next(err);
   }
 });
 
-router.get('/api/coffeeQtyData', async (req, res, next)=> {
+router.get("/api/coffeeQtyData", async (req, res, next) => {
   try {
-    const currentUserId = req.session.currentUser._id; 
-    const userLogs = await Log.find({ user : currentUserId });
+    const currentUserId = req.session.currentUser._id;
+    const userLogs = await Log.find({ user: currentUserId });
     const coffeeDatas = [];
-    userLogs.forEach(elm => coffeeDatas.push(elm.coffeeQty));
+    userLogs.forEach((elm) => coffeeDatas.push(elm.coffeeQty));
     res.json(coffeeDatas);
-  } catch(err) {
-    next(err)
+  } catch (err) {
+    next(err);
   }
 });
 
